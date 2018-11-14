bee8670e3a9e28e4596f9c268ed923a8957f32c7
Amruth Pillai <im.amruth@gmail.com>
2018-11-15 02:42:44 +0530

    add cors compatibility

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const routes = require('./app/routes');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
